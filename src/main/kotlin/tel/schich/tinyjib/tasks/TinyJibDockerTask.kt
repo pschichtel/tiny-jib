0f2fb14c04cedba1527e49bb0070fae9fb9b4559
Phillip Schichtel <phillip@schich.tel>
2026-02-09 21:58:37 +0100

    the docker task needs the docker format

--- src/main/kotlin/tel/schich/tinyjib/tasks/TinyJibDockerTask.kt
+++ src/main/kotlin/tel/schich/tinyjib/tasks/TinyJibDockerTask.kt
@@ -2,6 +2,7 @@ package tel.schich.tinyjib.tasks
 
 import com.google.cloud.tools.jib.api.Containerizer
 import com.google.cloud.tools.jib.api.DockerDaemonImage
+import com.google.cloud.tools.jib.api.buildplan.ImageFormat
 import org.gradle.api.tasks.TaskAction
 import tel.schich.tinyjib.TinyJibExtension
 import tel.schich.tinyjib.TinyJibTask
@@ -11,6 +12,7 @@ abstract class TinyJibDockerTask @Inject constructor(extension: TinyJibExtension
     @TaskAction
     fun performAction() {
         val builder = setupBuilder()
+            .setFormat(ImageFormat.Docker)
         val targetImage = DockerDaemonImage.named(targetImageName())
         targetImage.setDockerEnvironment(emptyMap())
         val containerizer = Containerizer.to(targetImage)
