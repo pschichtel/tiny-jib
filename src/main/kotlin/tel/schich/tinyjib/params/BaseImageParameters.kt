9ff8f4b76e9b4cddd672ff2425833e2f44102bdf
Phillip Schichtel <phillip@schich.tel>
2026-02-10 00:29:04 +0100

    add builds for these lists

--- src/main/kotlin/tel/schich/tinyjib/params/BaseImageParameters.kt
+++ src/main/kotlin/tel/schich/tinyjib/params/BaseImageParameters.kt
@@ -7,14 +7,34 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.Optional
 import org.gradle.kotlin.dsl.listProperty
+import org.gradle.kotlin.dsl.newInstance
 import org.gradle.kotlin.dsl.property
 import javax.inject.Inject
 
-abstract class BaseImageParameters @Inject constructor(objectFactory: ObjectFactory) : ImageParams(objectFactory) {
+interface PlatformsBuilder {
+    fun platform(block: PlatformParameters.() -> Unit)
+}
+
+private class SimplePlatformsBuilder(
+    private val platforms: ListProperty<PlatformParameters>,
+    private val objectFactory: ObjectFactory,
+) : PlatformsBuilder {
+    override fun platform(block: PlatformParameters.() -> Unit) {
+        val instance = objectFactory.newInstance<PlatformParameters>()
+        instance.block()
+        platforms.add(instance)
+    }
+}
+
+abstract class BaseImageParameters @Inject constructor(private val objectFactory: ObjectFactory) : ImageParams(objectFactory) {
     @Input
     val image: Property<String> = objectFactory.property()
 
     @get:Optional
     @get:Nested
     val platforms: ListProperty<PlatformParameters> = objectFactory.listProperty()
+
+    fun platforms(block: PlatformsBuilder.() -> Unit) {
+        SimplePlatformsBuilder(platforms, objectFactory).block()
+    }
 }
--- src/main/kotlin/tel/schich/tinyjib/params/ExtraDirectoriesParameters.kt
+++ src/main/kotlin/tel/schich/tinyjib/params/ExtraDirectoriesParameters.kt
@@ -6,12 +6,32 @@ import org.gradle.api.provider.MapProperty
 import org.gradle.api.tasks.Input
 import org.gradle.kotlin.dsl.listProperty
 import org.gradle.kotlin.dsl.mapProperty
+import org.gradle.kotlin.dsl.newInstance
 import javax.inject.Inject
 
-abstract class ExtraDirectoriesParameters @Inject constructor(objectFactory: ObjectFactory) {
+interface PathsBuilder {
+    fun path(block: ExtraDirectoryParameters.() -> Unit)
+}
+
+private class SimplePathsBuilder(
+    private val paths: ListProperty<ExtraDirectoryParameters>,
+    private val objectFactory: ObjectFactory,
+) : PathsBuilder {
+    override fun path(block: ExtraDirectoryParameters.() -> Unit) {
+        val instance = objectFactory.newInstance<ExtraDirectoryParameters>()
+        instance.block()
+        paths.add(instance)
+    }
+}
+
+abstract class ExtraDirectoriesParameters @Inject constructor(private val objectFactory: ObjectFactory) {
     @get:Input
     val paths: ListProperty<ExtraDirectoryParameters> = objectFactory.listProperty()
 
     @get:Input
     val permissions: MapProperty<String, String> = objectFactory.mapProperty()
+
+    fun paths(block: PathsBuilder.() -> Unit) {
+        SimplePathsBuilder(paths, objectFactory).block()
+    }
 }
