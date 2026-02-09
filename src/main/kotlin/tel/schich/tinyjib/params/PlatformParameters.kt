8ea834de818343e9320a7b160047bd6e1c8e56bf
Phillip Schichtel <phillip@schich.tel>
2026-02-09 21:27:50 +0100

    more input annotations

--- src/main/kotlin/tel/schich/tinyjib/params/PlatformParameters.kt
+++ src/main/kotlin/tel/schich/tinyjib/params/PlatformParameters.kt
@@ -2,10 +2,14 @@ package tel.schich.tinyjib.params
 
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.kotlin.dsl.property
 import javax.inject.Inject
 
 abstract class PlatformParameters @Inject constructor(objectFactory: ObjectFactory) {
+    @Input
     val os: Property<String> = objectFactory.property()
+
+    @Input
     val architecture: Property<String> = objectFactory.property()
 }
